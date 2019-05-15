4de7385fd8080723933591944bf595770ec27f99
Beto <beto_2613@hotmail.com>
2019-05-15 10:12:49 +0200

    Create comp3.vue

---
+++ comp3.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Component 3</h2>
+    <ul>
+        <li>
+            <router-link to="/">Home</router-link>
+        </li>
+    </ul>
+</div>
+</template>
+<script>
+  export default {
+    
+  }
+</script>
